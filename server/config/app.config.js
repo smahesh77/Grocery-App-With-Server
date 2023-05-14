39dad541e34b35d64f97c647661822ff87c8b53f
S Mahesh <98635199+smahesh77@users.noreply.github.com>
2023-05-15 00:07:39 +0530

    Update app.config.js

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -1,5 +1,5 @@
 const Mongo_config = {
-    DB:"mongodb+srv://smahesh7736:x!_WAQ!NMYfy23N@cluster0.p9vxhp1.mongodb.net/GroceryDB?retryWrites=true&w=majority",
+    DB:"mongodb+srv://ter0.p9vxhp1.mongodb.net/GroceryDB?retryWrites=true&w=majority",
     PAGE_SIE: 10
 }
 
